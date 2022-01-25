b8faab5574c22f89742322c1284360a097b44c69
Dhurav72 <dhurav.bcastudent20.6258@cimage.in>
2022-01-25 23:53:25 +0530

    dashboard design

--- MARBLE SHOP APPLICATION/src/main/java/com/shop/management/Controller/Dashboard.java
+++ MARBLE SHOP APPLICATION/src/main/java/com/shop/management/Controller/Dashboard.java
@@ -2,34 +2,51 @@ package com.shop.management.Controller;
 
 import com.shop.management.util.DBConnection;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 
 import java.net.URL;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Properties;
 import java.util.ResourceBundle;
 
 public class Dashboard implements Initializable {
 
+    public HBox dashboard_main_container;
+    public VBox side_menu;
+    public VBox screen_container;
+    public Hyperlink menu_btn;
     private Connection connection;
     private DBConnection dbConnection;
+    private Properties properties;
 
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         dbConnection = new DBConnection();
-       connection = dbConnection.getConnection();
+
         try {
-            PreparedStatement ps = connection.prepareStatement("select * from tbl_role");
+            connection = dbConnection.getConnection();
+            properties =dbConnection.getProperties("query.properties");
+            PreparedStatement ps = connection.prepareStatement(properties.getProperty("Sidemanu"));
             ResultSet rs = ps.executeQuery();
-
             while (rs.next()){
-                System.out.println(rs.getString("role"));
+                System.out.println(rs.getString("menu_name"));
+                menu_btn.setText(rs.getString("menu_name"));
             }
-
         } catch (SQLException e) {
             e.printStackTrace();
+            System.out.println(e.getMessage());
+        }finally {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
