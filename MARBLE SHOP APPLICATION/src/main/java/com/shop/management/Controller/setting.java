3f50290b1484be4f324cb226b21ad57f567416f0
Dhurav72 <dhurav.bcastudent20.6258@cimage.in>
2022-02-03 20:26:52 +0530

    added setting.class

--- MARBLE SHOP APPLICATION/src/main/java/com/shop/management/Controller/setting.java
+++ MARBLE SHOP APPLICATION/src/main/java/com/shop/management/Controller/setting.java
@@ -1,4 +1,154 @@
 package com.shop.management.Controller;
 
-public class setting {
+import com.shop.management.Method.Method;
+import com.shop.management.util.DBConnection;
+import javafx.collections.ObservableList;
+import javafx.collections.ObservableListBase;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Properties;
+import java.util.ResourceBundle;
+
+public class setting implements Initializable {
+    public ComboBox theme;
+    public ComboBox fontStyle;
+    public ComboBox billing;
+    public ComboBox igstTax;
+    public ComboBox cgstTax;
+    public ComboBox sgstTax;
+    public Button saveBtn;
+    Connection conn=null;
+    DBConnection dbConnection=new DBConnection();
+    Method method=new Method();
+    Properties prop;
+    ObservableList<String>themeList;
+    ObservableList<String>fontStyleList;
+    ObservableList<Double>igstTaxList;
+    ObservableList<Double>cgstTaxList;
+    ObservableList<Double>sgstTaxList;
+    ObservableList<String>billingTypeList;
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        setThemeAndFontList();
+        setIgstCgstSgst();
+        setBillingType();
+        setSettingValue();
+        System.out.println(theme.getValue());
+
+    }
+//    set item add in theme and fontstle DropDwon
+    private void setThemeAndFontList(){
+        themeList=theme.getItems();
+        fontStyleList=fontStyle.getItems();
+        theme.setEditable(true);
+        fontStyle.setEditable(true);
+        try {
+            conn= dbConnection.getConnection();
+            prop=method.getProperties("query.properties");
+            PreparedStatement stmt=conn.prepareStatement(prop.getProperty("SET_THEME_FONT_LIST"));
+            ResultSet rs=stmt.executeQuery();
+            while (rs.next()){
+                themeList.add(rs.getString("theme_name"));
+                fontStyleList.add(rs.getString("font_name"));
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }finally {
+            try {
+                conn.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    // set item value add in tax dropdoewn
+
+    private  void  setIgstCgstSgst(){
+        igstTaxList=igstTax.getItems();
+        cgstTaxList=cgstTax.getItems();
+        sgstTaxList=sgstTax.getItems();
+        igstTax.setEditable(true);
+        cgstTax.setEditable(true);
+        sgstTax.setEditable(true);
+        try {
+            conn=dbConnection.getConnection();
+            prop=method.getProperties("query.properties");
+            PreparedStatement stmt=conn.prepareStatement(prop.getProperty("SET_IGST_CGST_SGST_List"));
+            ResultSet rs=stmt.executeQuery();
+            while (rs.next()){
+                igstTaxList.add(rs.getDouble("igst"));
+                cgstTaxList.add(rs.getDouble("cgst"));
+                sgstTaxList.add(rs.getDouble("sgst"));
+
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }finally {
+            try {
+                conn.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    //set item bill type
+    private  void setBillingType(){
+        billingTypeList=billing.getItems();
+        billing.setEditable(true);
+        billingTypeList.addAll("Non Gst Bill","Gst Bill","Stander Bill","On Diamond Bil");
+    }
+
+    private  void setSettingValue(){
+        try {
+            conn=dbConnection.getConnection();
+            prop=method.getProperties("query.properties");
+            PreparedStatement stmt=conn.prepareStatement(prop.getProperty("SET_VALUE"));
+            ResultSet rs=stmt.executeQuery();
+            while (rs.next()){
+                theme.setValue(rs.getString("theme"));
+                fontStyle.setValue(rs.getString("font_style"));
+                billing.setValue(rs.getString("billing_type"));
+                igstTax.setValue(rs.getDouble("igst_tax"));
+                sgstTax.setValue(rs.getDouble("sgst_tax"));
+                cgstTax.setValue(rs.getDouble("cgst_tax"));
+
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }finally {
+            try {
+                conn.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+   public void updateSetting(){
+        try {
+            conn=dbConnection.getConnection();
+            prop=method.getProperties("query.properties");
+            PreparedStatement stmt=conn.prepareStatement(prop.getProperty("UpdateSetting"));
+            stmt.setString(1,theme.getValue().toString());
+            stmt.setString(2,fontStyle.getValue().toString());
+            stmt.setString(3,billing.getValue().toString());
+            stmt.setDouble(4, (Double) igstTax.getValue());
+           stmt.setDouble(5, (Double) cgstTax.getValue());
+           stmt.setDouble(6, (Double) sgstTax.getValue());
+            stmt.executeUpdate();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
+
